86163b67df28183dc7c82758f1c9e807a374145d
Fabian <lindgrenfabian@gmail.com>
2021-10-31 20:25:55 +0100

    Add method to download answer data

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 // Get websockets
 function init() {
-    sockets = [];
-    var nativeWebSocket = window.WebSocket;
-    window.WebSocket = function (...args) {
-        const socket = new nativeWebSocket(...args);
-        sockets.push(socket);
-        return socket;
-    };
-    print(`Searching for websockets. You can now join the StarQuiz.`)
-    checkSockets();
-}
+    getJSON('https://raw.githubusercontent.com/LiterallyFabian/StarQuizSolver/master/app/answers.json',
+        function (err, data) {
+            if (err !== null) {
+                alert('Something went wrong: ' + err);
+            } else {
+                answers = data;
+
+                sockets = [];
+                var nativeWebSocket = window.WebSocket;
+                window.WebSocket = function (...args) {
+                    const socket = new nativeWebSocket(...args);
+                    sockets.push(socket);
+                    return socket;
+                };
+                print(`Searching for websockets. You can now join the StarQuiz.`)
+                checkSockets();
+            }
+        }
+    )
+};
 
 // Wait until user joins the StarQuiz
 function checkSockets() {
@@ -32,3 +42,19 @@ function runGame(ws) {
 function print(str) {
     console.log(`%c${str}`, "color: red; font-family: sans-serif; font-size: 4.5em; font-weight: bolder; text-shadow: #000 1px 1px;");
 }
+
+// grab json without jquery
+var getJSON = function (url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
