01ff42bc5b758457fd488b9515dff9e74dce3003
Fabian Lindgren <lindgrenfabian@gmail.com>
2021-11-20 19:16:52 +0100

    Print answers to console

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
-// Get websockets
-function init() {
+// Get websockets 
+function init(lang) {
 
-    // Download answers
-    getJSON('https://raw.githubusercontent.com/LiterallyFabian/auto-starquiz/master/answers.json',
+    // get the localization data
+    get(`https://msp2-static.mspcdns.com/translations/multiplayergames/quiz/${lang}/localization_data.txt`, 'text',
         function (err, data) {
             if (err !== null) {
                 alert('Something went wrong: ' + err);
             } else {
-                // Save answer data
+                // Save the localization data 
+                localization = {};
+
+
+                data.split("\r").forEach(d => {
+                    if (d.includes("ANSWER")) {
+                        // get the question ID
+                        let match = /Q(\d{0,4})_ANSWER(\d)=(.+)/g.exec(d);
+                        let questionID = match[1];
+                        let answerIndex = match[2];
+                        let answerText = match[3];
+
+                        // save the answer
+                        if (!localization.hasOwnProperty(questionID)) {
+                            localization[questionID] = {};
+                        }
+
+                        // -1 because the other answer array is 0-indexed
+                        localization[questionID][answerIndex - 1] = answerText;
+                    }
+                })
+
+                print("Answers saved.")
+            }
+        }
+    )
+
+    // Download answers 
+    get('https://raw.githubusercontent.com/LiterallyFabian/auto-starquiz/master/answers.json', 'json',
+        function (err, data) {
+            if (err !== null) {
+                alert('Something went wrong: ' + err);
+            } else {
+                // Save answer data 
                 answers = data;
 
-                // Start waiting for sockets
+                // Start waiting for sockets 
                 sockets = [];
                 var nativeWebSocket = window.WebSocket;
                 window.WebSocket = function (...args) {
@@ -25,7 +58,7 @@ function init() {
     )
 };
 
-// Wait until user joins the StarQuiz
+// Wait until user joins the StarQuiz 
 function checkSockets() {
     if (sockets.length === 0) {
         window.setTimeout(checkSockets, 100);
@@ -40,31 +73,26 @@ function runGame(ws) {
         console.log('Message from MSP: ', event.data);
         let match = /42.+quiz:chal.+QUIZ_.+Q(\d{0,3})_QUESTION/gm.exec(event.data);
 
-        // this is a question if my beautiful regex matches
+        // this is a question if my beautiful regex matches the message
         if (match != null) {
+            // Print the answer to the question
             let id = match[1];
             let ans = answers[id];
-            let ansDelay = parseInt(getRandomArbitrary(3000, 8000));
-            print(`Answering in ${ansDelay}ms.`)
-
-            setTimeout(function () {
-                print("Answered!")
-                ws.send(`42["quiz:answer",{"answer":${ans+1}}]`)
-            }, ansDelay);
+            print(`Answer: ${localization[id][ans]}`);
         }
     });
 }
 
-// print message that is easier to read
+// print message that is easier to read 
 function print(str) {
     console.log(`%c${str}`, "color: red; font-family: sans-serif; font-size: 4.5em; font-weight: bolder; text-shadow: #000 1px 1px;");
 }
 
-// grab json without jquery
-var getJSON = function (url, callback) {
+// grab data without jquery
+var get = function (url, responseType, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
-    xhr.responseType = 'json';
+    xhr.responseType = responseType;
     xhr.onload = function () {
         var status = xhr.status;
         if (status === 200) {
@@ -75,8 +103,3 @@ var getJSON = function (url, callback) {
     };
     xhr.send();
 };
-
-// get random number easily lol
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-}
